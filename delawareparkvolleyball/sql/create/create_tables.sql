e9948965c21ba1339eff32b5763665261d94b546
Tumb <thom.burnett@gmail.com>
2015-01-02 12:21:04 -0500

    Adding sql directory and a few files.

---
+++ delawareparkvolleyball/sql/create/create_tables.sql
@@ -0,0 +1,5 @@
+create table person
+(
+first_name varchar(50) ; 
+last_name varchar(50) ; 
+) ; 
--- delawareparkvolleyball/src/com/delawareparkvolleyball/schedule/League.java
+++ delawareparkvolleyball/src/com/delawareparkvolleyball/schedule/League.java
@@ -38,17 +38,17 @@ public class League {
 	}
 	
 	public String teamsTable() {
-		String html  = "<table>\n" ;
+		String html  = "<table border=\"1\">\n" ;
 		int teamCount = this.allTeams.size() ; 
 		for(int i = 0 ; i < teamCount ; i++) {
 			if(i % 2 == 0) {
-				html += "\t<tr {background: #CC0}>" ;
+				html += "\t<tr bgcolor=\"#ffff88\">" ;
 			}
 			else {
 				html += "\t<tr>" ; 
 			}
 				
-			html += "\t<tr> " ; //TODO: use  datatables.net  jquery plugin
+//			html += "\t<tr> " ; //TODO: use  datatables.net  jquery plugin
 			html += "\t\t<td>" + this.allTeams.get(i).getMan().getFirstName() + "</td>" ; 
 			html += "\t\t<td>" + this.allTeams.get(i).getWoman().getFirstName() + "</td>" ; 
 			html += "\t</tr> " ; 
