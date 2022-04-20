2b3989f89e3bcf1a195c760323e86583ccbc197e
Tumb <thom.burnett@gmail.com>
2022-04-20 14:27:46 -0400

    AddPlayersPane construction

--- ParkVballJava/src/view/AddPlayersPane.java
+++ ParkVballJava/src/view/AddPlayersPane.java
@@ -1,10 +1,45 @@
 package view;
 
 import controller.Controller;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 
 public class AddPlayersPane extends BorderPane {
 	
 	Controller controller ; 
+	
+	public AddPlayersPane(Controller controller) {
+		this.controller = controller ; 
+		
+		addBottomPane() ;
+		
+	}
+	
+	public void addBottomPane() {
+		GridPaneControlled bottomPane = new GridPaneControlled() ;
+		bottomPane.setController(this.controller) ;
+		bottomPane.setPadding(new Insets(10, 10, 10, 10));
+		bottomPane.setVgap(20);
+		bottomPane.setHgap(50);
+
+		Button cancelButton = new Button("Exit");
+		cancelButton.setStyle("-fx-background-color: pink; ") ;
+		EventHandler<MouseEvent> cancelEvent = new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				controller.returnToHomePane();
+			}
+		};
+		cancelButton.addEventFilter(MouseEvent.MOUSE_CLICKED, cancelEvent);
+
+		bottomPane.add(cancelButton, 1, 0) ; 
+		this.setBottom(bottomPane) ; 
+
+
+	}
+	
 
 }
